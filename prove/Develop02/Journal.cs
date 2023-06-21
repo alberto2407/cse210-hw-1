3a2f4d38e448cd6803ce6ab172121cf41d0f7dad
albertofc2407@gmail.com <alberto2407@gmail.com>
2023-06-21 12:55:38 +0200

    Journal Program

---
+++ prove/Develop02/Journal.cs
@@ -0,0 +1,44 @@
+
+public class Journal
+{
+    List<Entry>entries;
+
+    public Journal()
+    {
+        entries = new List<Entry>();
+    }
+
+    public void AddEntry(Entry newEntry)
+    {
+        entries.Add(newEntry);
+    }
+    public void DisplayAll()
+    {
+        foreach (Entry entry in entries)
+        {
+            entry.Display();
+            Console.WriteLine();
+        }
+    }
+    public void SaveToFile(string _file)
+    {
+        List<string> records = new List<string>();
+        foreach (Entry entry in entries)
+        {
+            string entryAsCVS = $"{entry.date}|{entry.promptText}|{entry.entryText}";
+            records.Add(entryAsCVS); 
+        }
+
+        File.WriteAllLines(_file, records);
+    }
+    public void LoadFromFile(string file)
+    {
+        List<string> records = File.ReadAllLines(file).ToList();
+        foreach(string record in records)
+        {
+            string[] splitString = record.Split('|');
+            Entry entry = new Entry(splitString[0], splitString[1], splitString[2]);
+            entries.Add(entry);
+        }
+    }
+}
--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -4,6 +4,56 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop02 World!");
+        Promptgenertor promptgenertor = new Promptgenertor();
+        Journal journal = new Journal();
+        int menuResponse = 0;
+        List<string> menu = new List<string>{
+            "Please select one of the follow choises:",
+            "1. Write",
+            "2. Display",
+            "3. Load ",
+            "4. Save",
+            "5. Quit",
+            "What would you like to do?"
+        };
+
+        Console.WriteLine("Welcome to the Journal Program.");
+
+        while (menuResponse!=5)
+        {
+            foreach (string line in menu)
+            {
+                Console.WriteLine(line);
+            };
+            menuResponse = Convert.ToInt32(Console.ReadLine());
+
+            switch (menuResponse)
+            {
+                case 1: 
+                    string prompt = promptgenertor.GetRandomPrompt();
+                    Console.WriteLine(prompt);
+                    Console.Write("> ");
+                    string response = Console.ReadLine();
+                    string date = DateTime.Now.Date.ToString("MMM,dd,yyyy");
+
+                    Entry entry = new Entry(date, prompt, response);
+                    journal.AddEntry(entry);
+                    break;
+                case 2:
+                    journal.DisplayAll();
+                    break;
+                case 3:
+                    Console.WriteLine("Name of file to load?");
+                    string fileNameLoad = Console.ReadLine();
+                    journal.LoadFromFile(fileNameLoad);
+                    break;
+                case 4:
+                    Console.WriteLine("What is the filename?");
+                    string filename = Console.ReadLine();
+                    journal.SaveToFile(filename);
+                    break;
+            }
+        } 
+    
     }
 }
